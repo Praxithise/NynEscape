b1113f28ded2805fa13f74fbd41e7c0c648fa0ef
Praxithise <jelle.santema@gmail.com>
2025-07-24 14:23:21 +0200

    change video and add gif

--- src/App.svelte
+++ src/App.svelte
@@ -16,16 +16,21 @@
 <main class={correct && "correct"}>
   {#if !correct}
     {#if guessed}
-      <video autoplay loop preload="auto">
-        <source src="nyn.mp4" type="video/mp4" />
-      </video>
+      <img
+        src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2xxc3Mzd3RjNm5pMW91OWl0YWd0cjM5eHIybzhtbnRnbHJsMTByOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Pzd4dARLvaoDK/giphy.gif"
+      />
     {/if}
     <form on:submit|preventDefault={checkAnswer}>
-      <input type="text" placeholder="wat is het woord?" bind:value={code} />
+      <input type="text" placeholder="vul in..." bind:value={code} />
       <button type="submit">Guess</button>
     </form>
   {:else}
-    <h1>Parels, dat zijn jullie stuk voor stuk!</h1>
+    <div>
+      <h1>Parels, dat zijn jullie stuk voor stuk!</h1>
+      <video autoplay loop preload="auto">
+        <source src="nyn.mp4" type="video/mp4" />
+      </video>
+    </div>
   {/if}
 </main>
 
@@ -73,6 +78,7 @@
   input {
     color: #022672;
     margin-bottom: 2rem;
+    max-width: 90%;
   }
   video {
     width: 90%;
